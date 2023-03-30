92d51d1add024709073ab9bb0fcb279b02f13009
Shaun Buck <shaunbuck1812@gmail.com>
2023-03-30 13:02:23 +0000

    Register new models in admin.py

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Guide, Event
 
-# Register your models here.
+
+@admin.register(Guide)
+class GuideAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'role')
+    search_fields = ['name']
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+
+    list_display = ('title', 'guide', 'event_date', 'start_time')
+    list_filter = ('event_date', 'start_time')
+    search_fields = ['title']
