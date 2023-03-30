96f7b8744eedc2dfea0185e9b3cc6ee68db524c1
Shaun Buck <shaunbuck1812@gmail.com>
2023-03-30 17:29:49 +0000

    Add basic views and url paths

--- events/views.py
+++ events/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .models import Guide, Event
 
-# Create your views here.
+
+def index(request):
+
+    template = 'index.html'
+
+    return render(request, template)
+
+
+def events(request):
+
+    upcoming_events = Event.objects.all()
+
+    template = 'events.html'
+    context = {
+        'events': upcoming_events
+    }
+
+    return render(request, template, context)
