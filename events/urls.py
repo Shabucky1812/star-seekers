b9dc0bf0622ac3c4ebe81a3c28c9c08fcda1c78c
Shaun Buck <shaunbuck1812@gmail.com>
2023-03-31 13:11:14 +0000

    Create event_details view and configure relevant url

--- events/urls.py
+++ events/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.events, name='events'),
+    path('<slug:event_id>', views.event_details, name=('event_details'))
 ]
--- events/views.py
+++ events/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Guide, Event
 
 
@@ -16,6 +16,18 @@ def events(request):
     template = 'events.html'
     context = {
         'events': upcoming_events
+    }
+
+    return render(request, template, context)
+
+
+def event_details(request, event_id):
+
+    event = get_object_or_404(Event, id=event_id)
+
+    template = 'event_details.html'
+    context = {
+        'event': event
     }
 
     return render(request, template, context)
