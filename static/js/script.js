17c2f4c39949dfb29f70872b4b25a59cf5efe428
Shaun Buck <shaunbuck1812@gmail.com>
2023-04-03 17:20:18 +0000

    Initialize materialize datepicker and timepicker

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,22 @@
-// materialize collapsible navbar initialization
 document.addEventListener('DOMContentLoaded', function() {
+    
+    // materialize collapsible navbar initialization
     var elems = document.querySelectorAll('.sidenav');
     M.Sidenav.init(elems);
+
+    // materialzie datepicker initialization
+    var elems = document.querySelectorAll('#id_event_date');
+    M.Datepicker.init(elems);
+
+    // materialzie timepicker initialization
+    var elems = document.querySelectorAll('#id_start_time');
+    M.Timepicker.init(elems);
+
+    // find add_event form event_date field and add datepicker class for materialzie functionality
+    dateField = document.getElementById('id_event_date')
+    dateField.setAttribute('class', 'datepicker')
+
+    // find add_event form start_time field and add timepicker class for materialzie functionality
+    dateField = document.getElementById('id_start_time')
+    dateField.setAttribute('class', 'timepicker')
 });
