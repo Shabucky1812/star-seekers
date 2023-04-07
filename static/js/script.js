4297f2f7e233d877bdb97c62772fe0b1ba2d121a
Shaun Buck <shaunbuck1812@gmail.com>
2023-04-07 18:55:04 +0000

    Create template booking form

--- static/js/script.js
+++ static/js/script.js
@@ -4,14 +4,15 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     M.Sidenav.init(elems);
 
-    // materialzie datepicker initialization
+    // materialize datepicker initialization
     var elems = document.querySelectorAll('#id_event_date');
     M.Datepicker.init(elems);
 
+    // materialize select initialization
     var elems = document.querySelectorAll('select');
     M.FormSelect.init(elems);
 
-    // find add_event form event_date field and add datepicker class for materialzie functionality
+    // find add_event form event_date field and add datepicker class for materialize functionality
     dateField = document.getElementById('id_event_date');
     if (dateField) {
         dateField.setAttribute('class', 'datepicker');
