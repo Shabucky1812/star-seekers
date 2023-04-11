70b14e4dbdf62f1e9274ceefada85f165b6163cc
Shaun Buck <shaunbuck1812@gmail.com>
2023-04-11 18:12:56 +0000

    Remove unnecessary line from urls

--- star_seekers/urls.py
+++ star_seekers/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from events.views import index
-from .views import handler403, handler404, handler405, handler500
 
 urlpatterns = [
     path('admin/', admin.site.urls),
