d52e19f15900323392537572c92291175e2209a5
Shabucky1812 <shaunbuck1812@gmail.com>
2023-04-16 09:17:20 +0000

    Create tests for each required event form field

--- events/test_forms.py
+++ events/test_forms.py
@@ -14,3 +14,45 @@ class TestEventForm(TestCase):
 
         form = EventForm({'title': ''})
         self.assertFalse(form.is_valid())
+        self.assertIn('title', form.errors.keys())
+        self.assertEqual(form.errors['title'][0], 'This field is required.')
+
+    def test_event_description_is_required(self):
+        """
+        Tests that the event's description field is required
+        """
+
+        form = EventForm({'description': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('description', form.errors.keys())
+        self.assertEqual(form.errors['description'][0], 'This field is required.')
+
+    def test_event_date_is_required(self):
+        """
+        Tests that the event's event_date field is required
+        """
+
+        form = EventForm({'event_date': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('event_date', form.errors.keys())
+        self.assertEqual(form.errors['event_date'][0], 'This field is required.')
+
+    def test_event_start_time_is_required(self):
+        """
+        Tests that the event's start_time field is required
+        """
+
+        form = EventForm({'start_time': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('start_time', form.errors.keys())
+        self.assertEqual(form.errors['start_time'][0], 'This field is required.')
+
+    def test_event_meet_point_is_required(self):
+        """
+        Tests that the event's meet_point field is required
+        """
+
+        form = EventForm({'meet_point': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('meet_point', form.errors.keys())
+        self.assertEqual(form.errors['meet_point'][0], 'This field is required.')
